34db5cbaa810f7bb05dd1489bf194974b7fa323a
Janica Solante <126395981+jbslnt@users.noreply.github.com>
2024-03-05 15:35:07 +0800

    first commit

---
+++ output.py
@@ -0,0 +1,18 @@
+# variable declaration
+x = 25
+message = "Hi po!"
+ratio = 3.14
+
+#checking the type of data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+#string formatting
+print(message + " my name is jessa. I am " +  str(x) + " years old")
+print(f"{message} my name is jessa. I am {x} years old")
+
+
+
+
+
